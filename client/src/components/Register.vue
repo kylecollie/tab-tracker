55cadaf392db3f621db00547ed6abdf8c5384519
Kyle Collie <kcollie@hotmail.com>
2018-06-09 09:57:08 -0400

    add signup nav

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,26 +1,33 @@
 <template>
-<v-layout column>
-  <v-flex xs6 offset-xs3>
-    <div class="white elevation-2">
-      <v-toolbar flat dense class="cyan" dark>
-        <v-toolbar-title>Register</v-toolbar-title>
-      </v-toolbar>
-      <div class="pl-4 pr-4 pt-2 pb-2">
-        <input type="email" name="email" v-model="email" placeholder="email" />
-        <br>
-        <input type="password" name="password" v-model="password" placeholder="password" />
-        <br>
-        <div class="error" v-html="error"></div>
-        <br>
-        <v-btn
-          class="cyan"
-          @click="register">
-          Register
-        </v-btn>
+  <v-layout align-center justify-center>
+    <v-flex xs6 >
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <v-text-field
+            label="Email"
+            v-model="email"
+          ></v-text-field>
+          <br>
+          <v-text-field
+            label="Password"
+            v-model="password"
+          ></v-text-field>
+          <br>
+          <div class="error" v-html="error"></div>
+          <br>
+          <v-btn
+            class="cyan"
+            dark
+            @click="register">
+            Register
+          </v-btn>
+        </div>
       </div>
-    </div>
-  </v-flex>
-</v-layout>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
