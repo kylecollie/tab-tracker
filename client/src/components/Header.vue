6a241d99361dbf9d2f1936c352630651af183f51
Kyle Collie <kcollie@hotmail.com>
2018-06-13 22:56:14 -0400

    add logout

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -23,6 +23,12 @@
         flat dark @click="navigateTo({name:'register'})">
           Sign Up
         </v-btn>
+
+        <v-btn
+        v-if="$store.state.isUserLoggedIn"
+        flat dark @click="logout">
+          Log Out
+        </v-btn>
       </v-toolbar-items>
     </v-toolbar>
 </template>
@@ -32,6 +38,13 @@ export default {
   methods: {
     navigateTo (route) {
       this.$router.push(route)
+    },
+    logout () {
+      this.$store.dispatch('setToken', null)
+      this.$store.dispatch('setUser', null)
+      this.$router.push({
+        name: 'root'
+      })
     }
   }
 }
