c818df7b10febe303cbee7e9f3819082a1a4303e
Kyle Collie <kcollie@hotmail.com>
2018-06-16 09:59:12 -0400

    add song service and view

--- client/src/components/songs.vue
+++ client/src/components/songs.vue
@@ -1,17 +1,32 @@
 <template>
   <v-layout align-center justify-center>
-    <v-flex xs6 >
+    <v-flex  >
       <panel title="Songs" >
+        <div v-for="song in songs" :key="song.id">
+          {{song.title}} -
+          {{song.artist}} -
+          {{song.album}}
+        </div>
       </panel>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import SongService from '@/services/SongsService'
 import Panel from '@/components/Panel'
 export default {
   components: {
     Panel
+  },
+  data () {
+    return {
+      songs: null
+    }
+  },
+  async mounted () {
+    // do a request to the backend for all the songs
+    this.songs = (await SongService.index()).data
   }
 }
 </script>
