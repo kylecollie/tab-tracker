4fb171604da81eb5e49aaec91325c08f93eecaea
Kyle Collie <kcollie@hotmail.com>
2018-06-16 23:09:33 -0400

    add song create and view

--- client/src/components/Panel.vue
+++ client/src/components/Panel.vue
@@ -1,9 +1,10 @@
 <template>
   <v-layout align-center justify-center>
-    <v-flex xs6 >
+    <v-flex>
       <div class="white elevation-2">
         <v-toolbar flat dense class="cyan" dark>
           <v-toolbar-title>{{title}}</v-toolbar-title>
+          <slot name="action" />
         </v-toolbar>
         <div class="pl-4 pr-4 pt-2 pb-2">
           <slot>
--- client/src/components/songs.vue
+++ client/src/components/songs.vue
@@ -1,11 +1,47 @@
 <template>
   <v-layout align-center justify-center>
-    <v-flex  >
-      <panel title="Songs" >
-        <div v-for="song in songs" :key="song.id">
-          {{song.title}} -
-          {{song.artist}} -
-          {{song.album}}
+    <v-flex>
+      <panel title="Songs">
+        <v-btn
+          slot="action"
+          @click="navigateTo({name: 'songs-create'})"
+          class="cyan accent-2"
+          light
+          small
+          absolute
+          right
+          middle
+          fab>
+          <v-icon>add</v-icon>
+        </v-btn>
+        <div v-for="song in songs" class="song" :key="song.id">
+          <v-layout>
+            <v-flex xs6>
+              <div class="song-title">
+                {{song.title}}
+              </div>
+              <div class="song-artist">
+                {{song.artist}}
+              </div>
+              <div class="song-genre">
+                {{song.genre}}
+              </div>
+              <v-btn
+                dark
+                class="cyan"
+                @click="navigateTo({
+                  name: 'song',
+                  params: {
+                    songId: song.id
+                  }
+                })">
+                View
+              </v-btn>
+            </v-flex>
+             <v-flex xs6>
+               <img class="album-image" :src="song.albumImageUrl" />
+            </v-flex>
+          </v-layout>
         </div>
       </panel>
     </v-flex>
@@ -24,6 +60,11 @@ export default {
       songs: null
     }
   },
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route);
+    }
+  },
   async mounted () {
     // do a request to the backend for all the songs
     this.songs = (await SongService.index()).data
@@ -32,4 +73,26 @@ export default {
 </script>
 
 <style scoped>
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
+
+.song-title {
+  font-size: 30px;
+}
+
+.song-artist {
+  font-size: 24px;
+}
+
+.song-genre {
+  font-size: 18px;
+}
+
+.album-image {
+  width: 70%;
+  margin: 0 auto;
+}
 </style>
